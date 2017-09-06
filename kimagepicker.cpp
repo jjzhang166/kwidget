9c6420331fd7d46878f19c6da0ba4487f0f6f248
wangjianling <wangjianling@hongkingsystem.cn>
2017-09-06 11:31:59 +0800

    图片选择器支持网络 + 本地两种方式

--- kimagepicker.cpp
+++ kimagepicker.cpp
@@ -1,9 +1,13 @@
 #include "stdafx.h"
 #include "kimagepicker.h"
+#include "kutil/misc.h"
 #include <QFileDialog>
+#include <QMouseEvent>
+#include <QInputDialog>
+#include "knet/net.h"
 
 KImagePicker::KImagePicker(QWidget *parent)
-    : QLabel(QStringLiteral("请点击选择图片"), parent)
+    : QLabel(QStringLiteral("鼠标左右键点选"), parent)
 {
     setFrameShape(QFrame::Box);
     setFrameShadow(QFrame::Raised);
@@ -14,24 +18,69 @@ KImagePicker::~KImagePicker()
 {
 }
 
-void KImagePicker::setPath(const QString& path)
+void KImagePicker::setImage(const QString& path)
 {
-    QPixmap pic;
-    if (pic.load(path)) {
-        path_ = path;
-        setPixmap(pic);
-    }
-    else {
-        setText(QStringLiteral("无效的图片！"));
-        path_ = "";
-    }
+	if (source_ == path) {
+		return;
+	}
+	source_ = path;
+
+	if (path.startsWith("http://", Qt::CaseInsensitive)
+		|| path.startsWith("https://", Qt::CaseInsensitive)) {
+		// 先下载图片
+		path_ = kutil::randomTempFileName();
+		setText(QStringLiteral("正在下载图片！"));
+		net::KHttpDownload(QUrl(path), path_, this,
+			SLOT(onDownload(int, QUrl, int, QNetworkReply::NetworkError)));
+	}
+	else {
+		path_ = path;
+		setLocalImage(path_);
+	}
 }
 
 void KImagePicker::mousePressEvent(QMouseEvent *ev)
 {
-    path_ = QFileDialog::getOpenFileName(this->window(), 
-        QStringLiteral("请选择图象文件"), "", "Image (*.jpg *.gif *.png)");
-    if (!path_.isEmpty()) {
-        setPath(path_);
-    }
+	QString path;
+	switch (ev->button())
+	{
+	case Qt::LeftButton:
+	{
+		if (image_mode_ & ImageModeLocalFile) {
+			path = QFileDialog::getOpenFileName(this->window(),
+				QStringLiteral("请选择图象文件"), path_, "Image (*.jpg *.gif *.png)");
+		}
+	}break;
+
+	case Qt::RightButton:
+	{
+		if (image_mode_ & ImageModeWebFile) {
+			path = QInputDialog().getText(this, QStringLiteral("请输入网络图片地址："),
+				QStringLiteral("请输入网络图片地址"), QLineEdit::Normal, source_);
+		}
+	}break;
+	}
+
+	if (!path.isEmpty()) {
+		setImage(path);
+	}
+}
+
+void KImagePicker::setLocalImage(const QString& path)
+{
+	QPixmap pic;
+	if (pic.load(path)) {
+		setPixmap(pic);
+	}
+	else {
+		setText(QStringLiteral("无效的图片！"));
+		path_ = "";
+	}
+}
+
+void KImagePicker::onDownload(int evt, QUrl, int progress, QNetworkReply::NetworkError err)
+{
+	if (net::HttpDownload_Event_Finished == evt) {
+		setLocalImage(path_);
+	}
 }
--- kimagepicker.h
+++ kimagepicker.h
@@ -1,24 +1,42 @@
 #pragma once
 
 #include <QLabel>
+#include <QNetworkReply>
 
 class KImagePicker : public QLabel
 {
     Q_OBJECT
 
 public:
+	enum ImageMode {
+		ImageModeLocalFile = 0x1,
+		ImageModeWebFile = 0x2,
+	};
+
     KImagePicker(QWidget *parent);
     ~KImagePicker();
 
-    QString path()const {
-        return path_;
+    inline QString source()const {
+        return source_;
     }
 
-    void setPath(const QString& path);
+	void setImage(const QString& path);
+
+	void setImageMode(int imgmode) {
+		image_mode_ = imgmode;
+	}
 
 protected:
     virtual void mousePressEvent(QMouseEvent *ev) Q_DECL_OVERRIDE;
 
+	// 不修改source/path，仅加载显示图片
+	void setLocalImage(const QString& path);
+
+protected slots:
+	void onDownload(int evt, QUrl, int progress, QNetworkReply::NetworkError);
+
 private:
+	int image_mode_ = 0xFF;
+	QString source_;
     QString path_;
 };
